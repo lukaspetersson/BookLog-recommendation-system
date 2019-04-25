5cdcf8b87fb63d1b7708a9012fdc385c8f4e1986
eptrlks <lukas.petersson.1999@gmail.com>
2019-04-25 16:18:15 +0200

    restore model

--- booklog_rec_system.py
+++ booklog_rec_system.py
@@ -1,12 +1,14 @@
+#Getting data
 import pandas as pd
+from sklearn.model_selection import train_test_split
 dataset = pd.read_csv('dataset/ratings.csv')
 train, test = train_test_split(dataset, test_size=0.2, random_state=42)
 n_users = len(dataset.user_id.unique())
 n_books = len(dataset.book_id.unique())
 
-
+#Creating Embedding Model
 from keras.layers import Input, Embedding, Flatten, Dot, Dense
-from keras.models import Model
+from keras.models import Model, load_model
 book_input = Input(shape=[1], name="Book-Input")
 book_embedding = Embedding(n_books+1, 5, name="Book-Embedding")(book_input)
 book_vec = Flatten(name="Flatten-Books")(book_embedding)
@@ -14,12 +16,17 @@ user_input = Input(shape=[1], name="User-Input")
 user_embedding = Embedding(n_users+1, 5, name="User-Embedding")(user_input)
 user_vec = Flatten(name="Flatten-Users")(user_embedding)
 prod = Dot(name="Dot-Product", axes=1)([book_vec, user_vec])
-model = Model([user_input, book_input], prod)
-model.compile('adam', 'mean_squared_error')
+#model = Model([user_input, book_input], prod)
+#model.compile('adam', 'mean_squared_error')
 
-history = model.fit([train.user_id, train.book_id], train.rating, epochs=10, verbose=1)
-model.save('regression_model.h5')
+##Training the model
+#history = model.fit([train.user_id, train.book_id], train.rating, epochs=10, verbose=1)
+#model.save('regression_model.h5')
 
+##restoring existing model
+model = load_model("regression_model.h5")
+
+#Visualizing embeddings
 # Extract embeddings
 book_em = model.get_layer('Book-Embedding')
 book_em_weights = book_em.get_weights()[0]
@@ -35,6 +42,7 @@ tsne = TSNE(n_components=2, verbose=1, perplexity=40, n_iter=300)
 tnse_results = tsne.fit_transform(book_em_weights)
 sns.scatterplot(x=tnse_results[:,0], y=tnse_results[:,1])
 
+#Making Recommendations
 # Creating dataset for making recommendations for the first user
 book_data = np.array(list(set(dataset.book_id)))
 user = np.array([1 for i in range(len(book_data))])
