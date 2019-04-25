871f64f2a97b4c139ac68671a5e51c91c4322e0a
eptrlks <lukas.petersson.1999@gmail.com>
2019-04-25 14:58:23 +0200

    Getting data

--- booklog_rec_system.py
+++ booklog_rec_system.py
@@ -1,1 +1,4 @@
-test
+dataset = pd.read_csv('ratings.csv')
+train, test = train_test_split(dataset, test_size=0.2, random_state=42)
+n_users = len(dataset.user_id.unique())
+n_books = len(dataset.book_id.unique())
