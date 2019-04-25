00419ea14987921a42fa32bed5390b964dd76626
eptrlks <lukas.petersson.1999@gmail.com>
2019-04-25 16:29:28 +0200

    logs

--- booklog_rec_system.py
+++ booklog_rec_system.py
@@ -1,4 +1,4 @@
-#Getting data
+print("LOG: Getting data")
 import pandas as pd
 from sklearn.model_selection import train_test_split
 dataset = pd.read_csv('dataset/ratings.csv')
@@ -6,7 +6,7 @@ train, test = train_test_split(dataset, test_size=0.2, random_state=42)
 n_users = len(dataset.user_id.unique())
 n_books = len(dataset.book_id.unique())
 
-#Creating Embedding Model
+print("LOG: Creating Embedding Model")
 from keras.layers import Input, Embedding, Flatten, Dot, Dense
 from keras.models import Model, load_model
 book_input = Input(shape=[1], name="Book-Input")
@@ -20,13 +20,16 @@ prod = Dot(name="Dot-Product", axes=1)([book_vec, user_vec])
 #model.compile('adam', 'mean_squared_error')
 
 ##Training the model
+#print("LOG: Training the model")
 #history = model.fit([train.user_id, train.book_id], train.rating, epochs=10, verbose=1)
 #model.save('regression_model.h5')
 
-##restoring existing model
+##Restoring Existing Model
+print("LOG: Restoring existing model")
 model = load_model("regression_model.h5")
 
 #Visualizing embeddings
+print("LOG: Visualizing embeddings")
 # Extract embeddings
 book_em = model.get_layer('Book-Embedding')
 book_em_weights = book_em.get_weights()[0]
@@ -43,12 +46,16 @@ tnse_results = tsne.fit_transform(book_em_weights)
 sns.scatterplot(x=tnse_results[:,0], y=tnse_results[:,1])
 
 #Making Recommendations
+print("LOG: Making Recommendations")
 # Creating dataset for making recommendations for the first user
+import numpy as np
 book_data = np.array(list(set(dataset.book_id)))
 user = np.array([1 for i in range(len(book_data))])
 predictions = model.predict([user, book_data])
 predictions = np.array([a[0] for a in predictions])
 recommended_book_ids = (-predictions).argsort()[:5]
+
+print("LOG: Print Recommendations")
 print(recommended_book_ids)
 print(predictions[recommended_book_ids])
 
@@ -56,3 +63,5 @@ books = pd.read_csv("dataset/books.csv")
 books.head()
 
 print(books[books["id"].isin(recommended_book_ids)])
+
+print("LOG: final")
