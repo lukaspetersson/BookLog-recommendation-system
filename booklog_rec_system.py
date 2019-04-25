40de7036093c82a8182b7bee2053970878e91406
eptrlks <lukas.petersson.1999@gmail.com>
2019-04-25 14:58:47 +0200

    Creating Embedding Model

--- booklog_rec_system.py
+++ booklog_rec_system.py
@@ -2,3 +2,16 @@ dataset = pd.read_csv('ratings.csv')
 train, test = train_test_split(dataset, test_size=0.2, random_state=42)
 n_users = len(dataset.user_id.unique())
 n_books = len(dataset.book_id.unique())
+
+
+from keras.layers import Input, Embedding, Flatten, Dot, Dense
+from keras.models import Model
+book_input = Input(shape=[1], name="Book-Input")
+book_embedding = Embedding(n_books+1, 5, name="Book-Embedding")(book_input)
+book_vec = Flatten(name="Flatten-Books")(book_embedding)
+user_input = Input(shape=[1], name="User-Input")
+user_embedding = Embedding(n_users+1, 5, name="User-Embedding")(user_input)
+user_vec = Flatten(name="Flatten-Users")(user_embedding)
+prod = Dot(name="Dot-Product", axes=1)([book_vec, user_vec])
+model = Model([user_input, book_input], prod)
+model.compile('adam', 'mean_squared_error')
