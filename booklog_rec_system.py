80931d279e9d056ced8e56b4fe7a739aabe267d9
Lukas <Lukas.petersson.1999@gmail.com>
2019-05-24 19:10:33 +0200

    restore

--- booklog_rec_system.py
+++ booklog_rec_system.py
@@ -55,25 +55,25 @@ print("LOG: Making Recommendations")
 # Creating dataset for making recommendations for the first user
 import numpy as np
 book_data = np.array(list(set(dataset.book_id)))
-user = np.array([0 for i in range(len(book_data))])
+user = np.array([1 for i in range(len(book_data))])
 
-books_read = [2,18,21,23,24]
-books = pd.read_csv("books.csv")
-books.head()
-#print(books[books["id"].isin(books_read)])
+# books_read = [2,18,21,23,24]
+# books = pd.read_csv("books.csv")
+# books.head()
+# #print(books[books["id"].isin(books_read)])
+#
+# a = []
+#
+# for i in range(len(book_data)):
+#   if i in books_read:
+#     a.append(5)
+#   else:
+#     a.append(0)
+# print(a)
+#
+# a = np.array(a)
 
-a = []
-
-for i in range(len(book_data)):
-  if i in books_read:
-    a.append(5)
-  else:
-    a.append(0)
-print(a)
-
-a = np.array(a)
-
-predictions = model.predict([a, book_data])
+predictions = model.predict([user, book_data])
 predictions = np.array([a[0] for a in predictions])
 recommended_book_ids = (-predictions).argsort()[len(books_read):len(books_read)+5]
 
